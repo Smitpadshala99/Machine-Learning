200d26cb27134e7881d2906f5802013ea89ac8ed
Smit Padshala <112695070+Smitpadshala99@users.noreply.github.com>
2023-07-21 12:38:46 +0530

    Add files via upload

---
+++ Ep3/dogs.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+grey = 500
+labs = 500
+
+grey_height = 28 + 4 * np.random.randn(grey)
+labs_height = 24 + 4 * np.random.randn(labs)
+
+plt.hist([grey_height, labs_height], stacked=True, color=['r','b'])
+plt.show()
+
